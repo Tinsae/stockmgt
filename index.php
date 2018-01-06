d38fd8b5c9501532dc141a5a63f44e54d41eab72
Tinsae Gizachew <tigial3535@gmail.com>
2018-01-06 17:08:39 +0300

    again relative url

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ include_once('connect.php');
 	$result=mysqli_query($dbConn,$query);
 	if(mysqli_num_rows($result)>0)
 	  {
-		  echo "found results"; 
+		  $message = "found results";
+			echo ("<script type='text/javascript'>alert('$message');</script>");
 		$row=mysqli_fetch_array($result);
 		$_SESSION['uname']=$row['user_name'];
 		$_SESSION['pword']=$row['password'];
