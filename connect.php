ddeaff60ac9376092f24e117c4bc5b2142e65f8c
Tinsae Gizachew <tigial3535@gmail.com>
2018-01-06 16:40:03 +0300

    no include once anymore

--- connect.php
+++ connect.php
@@ -1,4 +1,54 @@
 <?php
+
+$sqlFileToExecute = 'stockmgt.sql';
+$hostname = 'us-cdbr-gcp-east-01.cleardb.net';
+$db_user = 'bf7fc733d29771';
+$db_password = '980e67efe15a403';
+$link = mysql_connect($hostname, $db_user, $db_password);
+if (!$link) {
+  die ("MySQL Connection error");
+}
+ 
+$database_name = 'gcp_83c58c04d2606dd50304';
+mysql_select_db($database_name, $link) or die ("Wrong MySQL Database");
+ 
+// read the sql file
+$f = fopen($sqlFileToExecute,"r+");
+$sqlFile = fread($f, filesize($sqlFileToExecute));
+$sqlArray = explode(';',$sqlFile);
+foreach ($sqlArray as $stmt) {
+  if (strlen($stmt)>3 && substr(ltrim($stmt),0,2)!='/*') {
+    $result = mysql_query($stmt);
+    if (!$result) {
+      $sqlErrorCode = mysql_errno();
+      $sqlErrorText = mysql_error();
+      $sqlStmt = $stmt;
+      break;
+    }
+  }
+}
+if ($sqlErrorCode == 0) {
+  echo "Script is executed succesfully!";
+} else {
+  echo "An error occured during installation!<br/>";
+  echo "Error code: $sqlErrorCode<br/>";
+  echo "Error text: $sqlErrorText<br/>";
+  echo "Statement:<br/> $sqlStmt<br/>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //$dbConn =mysqli_connect('localhost', 'root', '') or die("could not connect to server");
 $dbConn = mysqli_connect('us-cdbr-gcp-east-01.cleardb.net', 'bf7fc733d29771', '980e67efe15a403') or die("tinsae: could not connect to server");
 //mysqli_select_db($dbConn,'stockmgt') or die("could not select db stock mgt");
--- stockmgt.sql
+++ stockmgt.sql
@@ -1,17 +1,4 @@
-/*
-Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50067
-Source Host           : localhost:3306
-Source Database       : stockmgt
-
-Target Server Type    : MYSQL
-Target Server Version : 50067
-File Encoding         : 65001
-
-Date: 2016-03-12 15:02:12
-*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
