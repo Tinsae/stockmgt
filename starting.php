4d6c84c712265af9be6c50c46434d4a7694e9fe0
Tinsae Gizachew <tigial3535@gmail.com>
2018-01-06 16:33:14 +0300

    changed script totally

--- starting.php
+++ starting.php
@@ -1,31 +1,38 @@
 <?php
-$sql_file = 'stockmgt.sql';
-
-$contents = file_get_contents($sql_file);
-
-// Remove C style and inline comments
-$comment_patterns = array('/\/\*.*(\n)*.*(\*\/)?/', //C comments
-                          '/\s*--.*\n/', //inline comments start with --
-                          '/\s*#.*\n/', //inline comments start with #
-                          );
-$contents = preg_replace($comment_patterns, "\n", $contents);
-
-//Retrieve sql statements
-$statements = explode(";\n", $contents);
-$statements = preg_replace("/\s/", ' ', $statements);
-
-require_once 'MDB2.php';
-
-$mdb2 =& MDB2::connect('mysql://usr:pw@localhost/dbnam');
-
-foreach ($statements as $query) {
-    if (trim($query) != '') {
-        echo 'Executing query: ' . $query . "\n";
-        $res = $mdb2->exec($query);
-
-        if (PEAR::isError($res)) {
-            die($res->getMessage());
-        }
-    }
+$sqlFileToExecute = 'stockmgt.sql';
+$hostname = 'us-cdbr-gcp-east-01.cleardb.net';
+$db_user = 'bf7fc733d29771';
+$db_password = '980e67efe15a403';
+$link = mysql_connect($hostname, $db_user, $db_password);
+if (!$link) {
+  die ("MySQL Connection error");
 }
+ 
+$database_name = 'gcp_83c58c04d2606dd50304';
+mysql_select_db($database_name, $link) or die ("Wrong MySQL Database");
+ 
+// read the sql file
+$f = fopen($sqlFileToExecute,"r+");
+$sqlFile = fread($f, filesize($sqlFileToExecute));
+$sqlArray = explode(';',$sqlFile);
+foreach ($sqlArray as $stmt) {
+  if (strlen($stmt)>3 && substr(ltrim($stmt),0,2)!='/*') {
+    $result = mysql_query($stmt);
+    if (!$result) {
+      $sqlErrorCode = mysql_errno();
+      $sqlErrorText = mysql_error();
+      $sqlStmt = $stmt;
+      break;
+    }
+  }
+}
+if ($sqlErrorCode == 0) {
+  echo "Script is executed succesfully!";
+} else {
+  echo "An error occured during installation!<br/>";
+  echo "Error code: $sqlErrorCode<br/>";
+  echo "Error text: $sqlErrorText<br/>";
+  echo "Statement:<br/> $sqlStmt<br/>";
+}
+ 
 ?>
