3e29d0f56ceb0ec4475a830149534c11d7e5ba90
Tinsae Gizachew <tigial3535@gmail.com>
2018-01-06 14:36:52 +0300

    fixed dbname problem

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
 //$dbConn =mysqli_connect('localhost', 'root', '') or die("could not connect to server");
-$dbConn =mysqli_connect('us-cdbr-gcp-east-01.cleardb.net/gcp_83c58c04d2606dd50304', 'bf7fc733d29771', '980e67efe15a403') or die("could not connect to server");
-mysqli_select_db($dbConn,'stockmgt') or die("could not select db stock mgt");
+$dbConn =mysqli_connect('us-cdbr-gcp-east-01.cleardb.net', 'bf7fc733d29771', '980e67efe15a403') or die("could not connect to server");
+//mysqli_select_db($dbConn,'stockmgt') or die("could not select db stock mgt");
+mysqli_select_db($dbConn,'gcp_83c58c04d2606dd50304') or die("could not select db stock mgt");
 ?>
