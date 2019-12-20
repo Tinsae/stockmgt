fbe67b41e619a992e176f91c19b3ec03a4541222
Tinsae <tigial3535@gmail.com>
2019-12-20 13:16:08 -0600

    updted

--- stockmgt.sql
+++ stockmgt.sql
@@ -1,32 +1,4 @@
-
-
 SET FOREIGN_KEY_CHECKS=0;
-
-use gcp_83c58c04d2606dd50304;
-
-
--- ----------------------------
--- Table structure for `assignmaterial`
--- ----------------------------
-DROP TABLE IF EXISTS `assignmaterial`;
-CREATE TABLE `assignmaterial` (
-  `sub_store_ID` varchar(40) NOT NULL default '',
-  `mat_ID` varchar(40) NOT NULL default '',
-  `assign_qua` int(11) default NULL,
-  `assign_date` date default NULL,
-  PRIMARY KEY  (`mat_ID`,`sub_store_ID`),
-  KEY `sub_store_ID` (`sub_store_ID`,`mat_ID`),
-  CONSTRAINT `FK_mat-assign` FOREIGN KEY (`mat_ID`) REFERENCES `material` (`mat_ID`),
-  CONSTRAINT `FK_substo-assign` FOREIGN KEY (`sub_store_ID`) REFERENCES `substore` (`sub_store_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of assignmaterial
--- ----------------------------
-INSERT INTO `assignmaterial` VALUES ('S-009', 'Mat-001', '3', '2016-01-12');
-INSERT INTO `assignmaterial` VALUES ('S-010', 'Mat-007', '2', '2015-06-30');
-INSERT INTO `assignmaterial` VALUES ('S-400', 'Mat-007', '90', '2015-06-30');
-INSERT INTO `assignmaterial` VALUES ('S-009', 'Mat-888', '100', '2015-06-19');
 
 -- ----------------------------
 -- Table structure for `material`
@@ -48,31 +20,6 @@ INSERT INTO `material` VALUES ('Mat-001', 'Altrovicali Comfortable', 'Chair', '1
 INSERT INTO `material` VALUES ('Mat-007', 'Dell Desktop Core i8', 'Computer', '12000', '100');
 INSERT INTO `material` VALUES ('Mat-888', 'Bravo Pasta', 'Food & Beverage', '32', '100');
 
--- ----------------------------
--- Table structure for `requestmaterial`
--- ----------------------------
-DROP TABLE IF EXISTS `requestmaterial`;
-CREATE TABLE `requestmaterial` (
-  `req_no` smallint(6) NOT NULL auto_increment,
-  `sub_store_ID` varchar(40) NOT NULL,
-  `mat_ID` varchar(40) default NULL,
-  `reqt_user` varchar(40) NOT NULL,
-  `appv_user` varchar(40) default NULL,
-  PRIMARY KEY  (`req_no`),
-  KEY `FK_user-request` (`reqt_user`),
-  KEY `FK_user-approve` (`appv_user`),
-  KEY `FK_req-assign` (`sub_store_ID`,`mat_ID`),
-  CONSTRAINT `FK_req-assign` FOREIGN KEY (`sub_store_ID`, `mat_ID`) REFERENCES `assignmaterial` (`sub_store_ID`, `mat_ID`),
-  CONSTRAINT `FK_user-approve` FOREIGN KEY (`appv_user`) REFERENCES `user` (`user_name`),
-  CONSTRAINT `FK_user-request` FOREIGN KEY (`reqt_user`) REFERENCES `user` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of requestmaterial
--- ----------------------------
-INSERT INTO `requestmaterial` VALUES ('1', 'S-009', 'Mat-001', 'ashu', 'yihune');
-INSERT INTO `requestmaterial` VALUES ('2', 'S-010', 'Mat-007', 'abebe', 'yihune');
-INSERT INTO `requestmaterial` VALUES ('3', 'S-009', 'Mat-888', 'henimagna', 'yihune');
 
 -- ----------------------------
 -- Table structure for `substore`
@@ -127,3 +74,56 @@ INSERT INTO `user` VALUES ('Kal', 'Solomon', 'F', 'Kal', 'masi', 'Customer', 'S-
 INSERT INTO `user` VALUES ('Seble', 'Mekonnen', 'M', 'seblisweet', 'sweet', 'Admin', null);
 INSERT INTO `user` VALUES ('Tinsae', 'Gizachew', 'M', 'tinsae', 'tinsu', 'Admin', null);
 INSERT INTO `user` VALUES ('Yihunelegne', 'Eneyew', 'M', 'yihune', 'yihune', 'Keeper', 'S-009');
+
+
+
+
+-- ----------------------------
+-- Table structure for `assignmaterial`
+-- ----------------------------
+DROP TABLE IF EXISTS `assignmaterial`;
+CREATE TABLE `assignmaterial` (
+  `sub_store_ID` varchar(40) NOT NULL default '',
+  `mat_ID` varchar(40) NOT NULL default '',
+  `assign_qua` int(11) default NULL,
+  `assign_date` date default NULL,
+  PRIMARY KEY  (`mat_ID`,`sub_store_ID`),
+  KEY `sub_store_ID` (`sub_store_ID`,`mat_ID`),
+  CONSTRAINT `FK_mat-assign` FOREIGN KEY (`mat_ID`) REFERENCES `material` (`mat_ID`),
+  CONSTRAINT `FK_substo-assign` FOREIGN KEY (`sub_store_ID`) REFERENCES `substore` (`sub_store_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of assignmaterial
+-- ----------------------------
+INSERT INTO `assignmaterial` VALUES ('S-009', 'Mat-001', '3', '2016-01-12');
+INSERT INTO `assignmaterial` VALUES ('S-010', 'Mat-007', '2', '2015-06-30');
+INSERT INTO `assignmaterial` VALUES ('S-400', 'Mat-007', '90', '2015-06-30');
+INSERT INTO `assignmaterial` VALUES ('S-009', 'Mat-888', '100', '2015-06-19');
+
+-- ----------------------------
+-- Table structure for `requestmaterial`
+-- ----------------------------
+DROP TABLE IF EXISTS `requestmaterial`;
+CREATE TABLE `requestmaterial` (
+  `req_no` smallint(6) NOT NULL auto_increment,
+  `sub_store_ID` varchar(40) NOT NULL,
+  `mat_ID` varchar(40) default NULL,
+  `reqt_user` varchar(40) NOT NULL,
+  `appv_user` varchar(40) default NULL,
+  PRIMARY KEY  (`req_no`),
+  KEY `FK_user-request` (`reqt_user`),
+  KEY `FK_user-approve` (`appv_user`),
+  KEY `FK_req-assign` (`sub_store_ID`,`mat_ID`),
+  CONSTRAINT `FK_req-assign` FOREIGN KEY (`sub_store_ID`, `mat_ID`) REFERENCES `assignmaterial` (`sub_store_ID`, `mat_ID`),
+  CONSTRAINT `FK_user-approve` FOREIGN KEY (`appv_user`) REFERENCES `user` (`user_name`),
+  CONSTRAINT `FK_user-request` FOREIGN KEY (`reqt_user`) REFERENCES `user` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of requestmaterial
+-- ----------------------------
+INSERT INTO `requestmaterial` VALUES ('1', 'S-009', 'Mat-001', 'ashu', 'yihune');
+INSERT INTO `requestmaterial` VALUES ('2', 'S-010', 'Mat-007', 'abebe', 'yihune');
+INSERT INTO `requestmaterial` VALUES ('3', 'S-009', 'Mat-888', 'henimagna', 'yihune');
+
